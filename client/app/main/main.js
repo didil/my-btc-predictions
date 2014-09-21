327702d43b489b2a4338a950a1f54ddf8846031b
didil <haritahadil@gmail.com>
2014-09-21 17:27:51 -0500

    init project

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('myBtcPredictionsApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
